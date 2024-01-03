c66ebe1376dc1225c133521f3ac67847cd9f9c66
Volkan Akyel <volkan@uikielle.com>
2024-01-03 23:48:21 +0800

    expense-Dash 📈 feat | update sidebar

---
+++ src/components/IncomeCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="bg-white border border-grey-400 rounded-md p-4 max-w-xs-card cursor-pointer"
+  >
+    <div class="flex justify-between items-center mb-2">
+      <div class="text-gray-700 text-l">Total Income</div>
+    </div>
+    <div class="text-black text-2xl">$974.99</div>
+    <div class="text-green-500 text-sm flex items-center">
+      <svg
+        class="fill-current text-green-500 mr-1"
+        xmlns="http://www.w3.org/2000/svg"
+        width="10"
+        height="8"
+        viewBox="0 0 10 8"
+        fill="none"
+      >
+        <path
+          d="M9.49498 1.57071C9.53403 1.29734 9.34408 1.04408 9.07071 1.00502L4.61594 0.368629C4.34257 0.329576 4.0893 0.519526 4.05025 0.792893C4.0112 1.06626 4.20115 1.31953 4.47452 1.35858L8.43431 1.92426L7.86863 5.88406C7.82958 6.15743 8.01953 6.4107 8.29289 6.44975C8.56626 6.4888 8.81953 6.29885 8.85858 6.02548L9.49498 1.57071ZM1.3 7.9L9.3 1.9L8.7 1.1L0.7 7.1L1.3 7.9Z"
+          fill="#3DD26A"
+        />
+      </svg>
+      <p class="text-grey-700">from last week 7.85%</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
