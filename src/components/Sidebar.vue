b0c9d7e055659e54f8438e5008a81efb71f288f0
Volkan Akyel <volkan@uikielle.com>
2024-01-26 00:48:59 +0800

    expense-Dash 📈 feat | update Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,13 +5,13 @@
   >
     <div class="flex justify-center items-center py-9 px-10">
       <img src="../assets/Symbol.png" alt="" />
-      <h1 class="capitalize text-white font-semibold">FinanceDashboard</h1>
+      <h1 class="capitalize text-white font-semibold">TravelDashboard</h1>
     </div>
     <div class="separator"></div>
     <ul class="flex flex-col mt-7 gap-8 pl-7">
       <li class="cursor-pointer hover:text-white">Dashboard</li>
       <li class="cursor-pointer hover:text-white">Analytics</li>
-      <li class="cursor-pointer hover:text-white">Payments</li>
+      <li class="cursor-pointer hover:text-white">Transaction</li>
       <li class="cursor-pointer hover:text-white">Deposit</li>
       <li class="cursor-pointer hover:text-white">Securities</li>
     </ul>
