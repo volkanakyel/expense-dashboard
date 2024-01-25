507b89c9be1c7d0b91dabefee0c29c17d42c9043
Volkan Akyel <volkan@uikielle.com>
2024-01-26 00:49:45 +0800

    expense-Dash 📈 feat | add currencyConverter Widget

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,78 @@ import Sidebar from "@/components/Sidebar.vue";
 import Navbar from "@/components/Navbar.vue";
 import IncomeCard from "@/components/IncomeCard.vue";
 import ExpenseItem from "@/components/ExpenseItem.vue";
+import CurrencyConverter from "@/components/CurrencyConverter.vue";
 import { useGeolocation } from "./composables/useGeolocation";
 import { ref, onMounted } from "vue";
 const restaurants = ref([]);
 const geoError = ref(null);
+const currenciesList = ref([
+  { code: "AUD", flag: "🇦🇺" }, // Australian Dollar
+  { code: "BGN", flag: "🇧🇬" }, // Bulgarian Lev
+  { code: "BRL", flag: "🇧🇷" }, // Brazilian Real
+  { code: "CAD", flag: "🇨🇦" }, // Canadian Dollar
+  { code: "CHF", flag: "🇨🇭" }, // Swiss Franc
+  { code: "CNY", flag: "🇨🇳" }, // Chinese Yuan
+  { code: "CZK", flag: "🇨🇿" }, // Czech Koruna
+  { code: "DKK", flag: "🇩🇰" }, // Danish Krone
+  { code: "EUR", flag: "🇪🇺" }, // Euro
+  { code: "GBP", flag: "🇬🇧" }, // British Pound Sterling
+  { code: "HKD", flag: "🇭🇰" }, // Hong Kong Dollar
+  { code: "HRK", flag: "🇭🇷" }, // Croatian Kuna
+  { code: "HUF", flag: "🇭🇺" }, // Hungarian Forint
+  { code: "IDR", flag: "🇮🇩" }, // Indonesian Rupiah
+  { code: "ILS", flag: "🇮🇱" }, // Israeli New Sheqel
+  { code: "INR", flag: "🇮🇳" }, // Indian Rupee
+  { code: "ISK", flag: "🇮🇸" }, // Icelandic Króna
+  { code: "JPY", flag: "🇯🇵" }, // Japanese Yen
+  { code: "KRW", flag: "🇰🇷" }, // South Korean Won
+  { code: "MXN", flag: "🇲🇽" }, // Mexican Peso
+  { code: "MYR", flag: "🇲🇾" }, // Malaysian Ringgit
+  { code: "NOK", flag: "🇳🇴" }, // Norwegian Krone
+  { code: "NZD", flag: "🇳🇿" }, // New Zealand Dollar
+  { code: "PHP", flag: "🇵🇭" }, // Philippine Peso
+  { code: "PLN", flag: "🇵🇱" }, // Polish Zloty
+  { code: "RON", flag: "🇷🇴" }, // Romanian Leu
+  { code: "RUB", flag: "🇷🇺" }, // Russian Ruble
+  { code: "SEK", flag: "🇸🇪" }, // Swedish Krona
+  { code: "SGD", flag: "🇸🇬" }, // Singapore Dollar
+  { code: "THB", flag: "🇹🇭" }, // Thai Baht
+  { code: "TRY", flag: "🇹🇷" }, // Turkish Lira
+  { code: "USD", flag: "🇺🇸" }, // US Dollar
+  { code: "ZAR", flag: "🇿🇦" }, // South African Rand
+]);
+const selectedCurrency = ref("AUD");
+const findCurrency = (code) => {
+  return currenciesList.value.find((currency) => currency.code === code) || {};
+};
+const getCurrencyList = async () => {
+  const apiKey = import.meta.env.VITE_CURRENCY_API;
+  const url = `https://api.freecurrencyapi.com/v1/currencies?apikey=${apiKey}&currencies=`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Error fetching currency List");
+    }
+    const data = await response.json();
+    console.log(data.data);
+    currenciesList.value = Object.keys(data.data).map(
+      (currency) => currency.code
+    );
+    //Object.keys(data);
+    // console.log(currenciesList.value);
+  } catch (error) {
+    console.error("Fetch error", error.message);
+  }
+};
 
 onMounted(async () => {
-  const { coords, error } = await useGeolocation();
-  geoError.value = error;
-
-  if (coords.latitude !== null && coords.longitude !== null) {
-    await fetchRestaurants(coords.latitude, coords.longitude);
-  }
+  // const { coords, error } = await useGeolocation();
+  // geoError.value = error;
+  // if (coords.latitude !== null && coords.longitude !== null) {
+  //   await fetchRestaurants(coords.latitude, coords.longitude);
+  // }
+  // await getCurrencyList();
 });
 
 async function fetchRestaurants(latitude, longitude) {
@@ -100,6 +160,27 @@ async function fetchRestaurants(latitude, longitude) {
             <h2 class="text-black font-bold text-xl mb-2">
               Currency converter
             </h2>
+            <!-- <div class="w-full max-w-xs mx-auto">
+              <div class="flex items-center">
+                <span v-if="selectedCurrency">{{
+                  findCurrency(selectedCurrency).flag
+                }}</span>
+                <select
+                  v-model="selectedCurrency"
+                  class="block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                >
+                  <option disabled value="">Select a currency</option>
+                  <option
+                    v-for="currency in currenciesList"
+                    :key="currency.code"
+                    :value="currency.code"
+                  >
+                    {{ currency.code }}
+                  </option>
+                </select>
+              </div>
+            </div> -->
+            <CurrencyConverter />
           </div>
           <!-- end currency converter -->
           <div class="max-w-sm bg-white p-6 rounded-md mb-10">
---
+++ src/components/CurrencyConverter.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="w-full max-w-xs mx-auto text-grey-700">
+    <div class="flex items-center justify-between my-4 bg-white rounded-md">
+      <div class="relative">
+        <button
+          @click="toggleFromDropdown"
+          class="flex items-center focus:outline-none"
+        >
+          <span class="mr-2" v-if="selectedCurrency">
+            {{ findCurrency(selectedCurrency).flag }}
+          </span>
+          <span>{{ selectedCurrency }}</span>
+          <span class="ml-2">▼</span>
+        </button>
+        <ul
+          v-show="dropdownFromOpen"
+          class="absolute z-10 w-32 py-1 mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-40 overflow-auto"
+          @click.away="dropdownFromOpen = false"
+        >
+          <li
+            v-for="currency in currenciesList"
+            :key="currency.code"
+            @click="selectFromCurrency(currency.code)"
+            class="block px-4 py-2 text-sm cursor-pointer hover:bg-gray-100"
+          >
+            {{ currency.flag }} {{ currency.code }}
+          </li>
+        </ul>
+      </div>
+      <input
+        type="text"
+        class="flex-1 px-2 py-1 ml-6 text-right border rounded-md focus:outline-none focus:border-blue-500"
+        placeholder="00.00"
+      />
+    </div>
+    <!-- Repeat the structure above for the 'Converted Amount' section -->
+    <div
+      class="flex items-center justify-between p-4 bg-white border border-gray-300 rounded-md mt-4"
+    >
+      <div class="relative">
+        <button
+          @click="toggleTargetDropdown"
+          class="flex items-center focus:outline-none"
+        >
+          <span class="mr-2" v-if="targetCurrency">{{
+            findCurrency(targetCurrency).flag
+          }}</span>
+          <span>{{ targetCurrency }}</span>
+          <span class="ml-2">▼</span>
+        </button>
+        <ul
+          v-show="dropdownTargetOpen"
+          class="absolute z-10 w-32 py-1 mt-1 bg-white border border-gray-300 rounded-md shadow-lg"
+          @click.away="dropdownOpen = false"
+        >
+          <li
+            v-for="currency in currenciesList"
+            :key="currency.code"
+            @click="selectTargetCurrency(currency.code)"
+            class="block px-4 py-2 text-sm cursor-pointer hover:bg-gray-100"
+          >
+            {{ currency.flag }} {{ currency.code }}
+          </li>
+        </ul>
+      </div>
+      <input
+        type="text"
+        class="flex-1 px-2 py-1 ml-2 text-right border rounded-md focus:outline-none focus:border-blue-500"
+        placeholder="1000.00"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const currenciesList = ref([
+  { code: "AUD", flag: "🇦🇺" }, // Australian Dollar
+  { code: "BGN", flag: "🇧🇬" }, // Bulgarian Lev
+  { code: "BRL", flag: "🇧🇷" }, // Brazilian Real
+  { code: "CAD", flag: "🇨🇦" }, // Canadian Dollar
+  { code: "CHF", flag: "🇨🇭" }, // Swiss Franc
+  { code: "CNY", flag: "🇨🇳" }, // Chinese Yuan
+  { code: "CZK", flag: "🇨🇿" }, // Czech Koruna
+  { code: "DKK", flag: "🇩🇰" }, // Danish Krone
+  { code: "EUR", flag: "🇪🇺" }, // Euro
+  { code: "GBP", flag: "🇬🇧" }, // British Pound Sterling
+  { code: "HKD", flag: "🇭🇰" }, // Hong Kong Dollar
+  { code: "HRK", flag: "🇭🇷" }, // Croatian Kuna
+  { code: "HUF", flag: "🇭🇺" }, // Hungarian Forint
+  { code: "IDR", flag: "🇮🇩" }, // Indonesian Rupiah
+  { code: "ILS", flag: "🇮🇱" }, // Israeli New Sheqel
+  { code: "INR", flag: "🇮🇳" }, // Indian Rupee
+  { code: "ISK", flag: "🇮🇸" }, // Icelandic Króna
+  { code: "JPY", flag: "🇯🇵" }, // Japanese Yen
+  { code: "KRW", flag: "🇰🇷" }, // South Korean Won
+  { code: "MXN", flag: "🇲🇽" }, // Mexican Peso
+  { code: "MYR", flag: "🇲🇾" }, // Malaysian Ringgit
+  { code: "NOK", flag: "🇳🇴" }, // Norwegian Krone
+  { code: "NZD", flag: "🇳🇿" }, // New Zealand Dollar
+  { code: "PHP", flag: "🇵🇭" }, // Philippine Peso
+  { code: "PLN", flag: "🇵🇱" }, // Polish Zloty
+  { code: "RON", flag: "🇷🇴" }, // Romanian Leu
+  { code: "RUB", flag: "🇷🇺" }, // Russian Ruble
+  { code: "SEK", flag: "🇸🇪" }, // Swedish Krona
+  { code: "SGD", flag: "🇸🇬" }, // Singapore Dollar
+  { code: "THB", flag: "🇹🇭" }, // Thai Baht
+  { code: "TRY", flag: "🇹🇷" }, // Turkish Lira
+  { code: "USD", flag: "🇺🇸" }, // US Dollar
+  { code: "ZAR", flag: "🇿🇦" }, // South African Rand
+]);
+const selectedCurrency = ref("USD"); // Default currency
+const targetCurrency = ref("EUR"); // Default currency
+const dropdownTargetOpen = ref(false);
+const dropdownFromOpen = ref(false);
+
+const toggleFromDropdown = () => {
+  dropdownFromOpen.value = !dropdownFromOpen.value;
+};
+
+const toggleTargetDropdown = () => {
+  dropdownTargetOpen.value = !dropdownTargetOpen.value;
+};
+
+const selectFromCurrency = (code) => {
+  selectedCurrency.value = code;
+  dropdownFromOpen.value = false;
+};
+
+const selectTargetCurrency = (code) => {
+  selectedCurrency.value = code;
+  dropdownFromOpen.value = false;
+};
+
+const findCurrency = (code) => {
+  return currenciesList.value.find((currency) => currency.code === code) || {};
+};
+</script>
+
+<style scoped></style>
